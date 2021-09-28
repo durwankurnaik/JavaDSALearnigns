279e8af6f470f9e14b403f875950086b9494bda3
Durwankur <ndurwankur14@gmail.com>
2021-09-28 09:18:46 +0530

    Coming back after a very long time.

---
+++ Strings/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Durwankur";
+        String herChoice = "Durwankur";
+
+        if (herChoice == name) {
+            System.out.println("She likes you dude.");
+        } else {
+            System.out.println("Maybe she doesn't want to be with you dude");
+        }
+    }
+}
