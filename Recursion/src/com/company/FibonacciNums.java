b4b11968f3b9b2cb9e54d1da171e52c20a42860b
Durwankur <ndurwankur14@gmail.com>
2021-09-28 20:43:02 +0530

    Updated @ 28 Sep 2021 20:42

--- Recursion/src/com/company/FibonacciNums.java
+++ Recursion/src/com/company/FibonacciNums.java
@@ -1,9 +1,9 @@
 package com.company;
-
+// This is not a efficient algorithm since it takes golden ratio to the power 2 as the time complexity.
 public class FibonacciNums {
 
     public static void main(String[] args) {
-        System.out.println(fibOfNum(7));
+        System.out.println(fibOfNum(6));
     }
 
     static int fibOfNum(int num) {
