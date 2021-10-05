35de934ab0d2c91eac33af9dabd1d3b458df344d
Durwankur <ndurwankur14@gmail.com>
2021-10-05 13:40:44 +0530

    Updated till 5 Oct 2021 @ 13:40

--- Complexities/src/com/company/Main.java
+++ Complexities/src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(fiboFormula(i));
+        }
+    }
 
+    static int fiboFormula(int n) {
+        // Time Complexity for this fibonacci numbers is O(1.618 ^ n) where 1.618 is also called as golden ratio.
+        return (int)(1 / Math.sqrt(5) * (Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)));
+        // The above formula is derived using "Akra Bazzi" Formula which is considered as the best way to derive recurrence relationships.
     }
 }
