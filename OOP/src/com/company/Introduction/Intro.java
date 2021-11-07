7edb99c23e29b4a6a5d2bf810373e593b6f1bad2
Durwankur Naik <ndurwankur14@gmail.com>
2021-11-07 20:26:42 +0530

    Some cool concepts of OOP

--- OOP/src/com/company/Introduction/Intro.java
+++ OOP/src/com/company/Introduction/Intro.java
@@ -2,13 +2,16 @@ package com.company.Introduction;
 
 public class Intro {
     public static void main(String[] args) {
-        Students Durwankur = new Students();
+        Students Durwankur = new Students(206, "Durwankur Naik", 88.45f);
+        Students Tulsi = new Students(207, "Harshit Naik", 90.69f);
 
-        Durwankur.marks = 84.456f;
-        Durwankur.name = "Durwankur";
-        Durwankur.roll_no = 206;
-
-        System.out.println(Durwankur);
+        System.out.println(Durwankur.roll_no);
+        System.out.println(Durwankur.name);
+        System.out.println(Durwankur.marks);
+        System.out.println();
+        System.out.println(Tulsi.roll_no);
+        System.out.println(Tulsi.name);
+        System.out.println(Tulsi.marks);
     }
 }
 
@@ -16,4 +19,10 @@ class Students {
     int roll_no;
     String name;
     float marks;
+
+    Students(int roll_no, String name, float marks) {
+        this.roll_no = roll_no;
+        this.name = name;
+        this.marks = marks;
+    }
 }
---
+++ OOP/src/com/company/Introduction/WrapperClass.java
@@ -0,0 +1,30 @@
+package com.company.Introduction;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer num = 50;
+
+        System.out.println(num);
+
+        A durwankur = new A("Durwankur");
+        durwankur = new A("Someone else's Durwankur");
+
+        System.out.println(durwankur.num);
+        System.out.println(durwankur.name);
+
+        final A rahul = new A("rahul");
+//        rahul = new A("Someone else's rahul"); This cant be done
+        rahul.name = "Somewhat different Rahul"; // This can be done because it is a non-primitive data-type
+
+//        Durwankur.num = 15; Can't do this because the int is final in this case
+    }
+}
+
+class A {
+    final int num = 69; // Can't even change the value of num because it is a primitive data-type
+    String name;
+
+    public A(String name) {
+        this.name = name;
+    }
+}
